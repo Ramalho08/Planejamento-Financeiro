3728c6de27d59ed38b209e045ee39ee2a0159285
Ramalho08 <guilhermeg240r@gmail.com>
2026-07-02 13:45:10 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-// Ramalho Finance Supabase Cloud READY
-window.RF_SUPABASE_URL = "https://lomottdedifvbgsfnvme.supabase.co";
-window.RF_SUPABASE_ANON_KEY = "sb_publishable_fm4_CoPlfB30_qOfjga6yQ_P0MaeRje";
